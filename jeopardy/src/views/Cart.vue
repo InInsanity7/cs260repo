6c9df5aedd8c6bbd1fe2209d4c45667b364d6b72
Nathan Poston <67882689+InInsanity7@users.noreply.github.com>
2021-03-21 13:56:29 -0600

    starting jeopardy

---
+++ jeopardy/src/views/Cart.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <h1>Your Cart</h1>
+    <p v-if="this.$root.$data.cartSize === 0">Your cart is empty. Go get the goods!</p>
+    <ProductList :products="cart" />
+</div>
+</template>
+
+<script>
+import ProductList from "../components/ProductList.vue"
+export default {
+  name: 'Browse',
+  components: {
+    ProductList
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    cart() {
+      return this.$root.$data.cart;
+    }
+  },
+}
+</script>
+
